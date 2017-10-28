c5ddc642ab34af7a6c58829828eb3c4c5b9e6bc7
Satya S Malladi <sarvanisatya@users.noreply.github.com>
2017-10-28 16:02:12 -0400

    read input file udpated

--- read_input.py
+++ read_input.py
@@ -10,3 +10,9 @@ import random
 import numpy as np
 from numpy.random import choice
 from gurobipy import*
+
+def read_input():
+    rd = []
+    with open('zdt1.lp', 'rb') as ex:
+        rd = list(csv.reader(ex, skipinitialspace=True))
+        rd = [i for i in rd if i]
