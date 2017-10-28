e95dea3fd82b8dcf00c0f3de789ee63b89376176
Clovis496 <32486948+Clovis496@users.noreply.github.com>
2017-10-29 00:29:58 +0200

    dzdz

---
+++ population.py
@@ -0,0 +1,58 @@
+# Definition of the population class
+
+
+import Solution.py
+
+class Population(object):
+
+    """
+    A population is a set of solutions. It has te following arguments:
+    list_of_solution : list of solutions of the population
+    ideal_Z : Ideal point of the solutions
+    """
+
+    def _init_(self, number_of_solution):
+
+        """
+        :param number_of_solution: The size of the population, how many solutions it will contain
+        :param list_of_solutions: the list of solution of the population
+        :param ideal_Z: the ideal point of the population (in objective space)
+        :return: create a new instance population
+        """
+
+        self.number_of_solution = number_of_solution
+
+        for i in range(self.number_of_solution):
+            sol = Solution(30, ['Continuous'] * 30, i)
+            sub_problem = ,,
+            while not sol.feasible:
+                sol = Solution(30, ['Continuous'] * 30, i)
+            if sub_problem not in self.list_of_subproblems:
+                self.list_of_subproblems.append(sub_problem)
+            self.list_of_solutions.append(sol)
+
+        self.ideal_Z = self.compute_ideal_z(self.list_of_solution)
+
+
+
+    def compute_ideal_z(list_of_solution):
+
+        """
+        Compute the values of the coordinates of the ideal point for the list of solution received
+        :param list_of_solution: A list of solution, which include decision space coordinates and objective values
+        :return: One solution which is the ideal point of the list of solution
+        """
+
+        ideal_Z  = [None] * len(list_of_solution[0].objective_val)
+
+        for solution in list_of_solution:
+            for value in solution.objective_val:
+                if ideal_Z[value] == None:
+                    ideal_Z[value] = value
+                if value < ideal_Z:
+                    ideal_Z[value] = value
+
+        return ideal_Z
+
+    Solution(30, ['Con'])
+
