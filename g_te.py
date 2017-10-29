f5f6f37a62f316bcfc522902e2fd5ad841dbffbb
Satya S Malladi <sarvanisatya@users.noreply.github.com>
2017-10-29 15:09:00 -0400

    g_te function

--- g_te.py
+++ g_te.py
@@ -1,3 +1,8 @@
 #!/opt/python/bin/python
 
-def g_te(xA,xB, ideal_z):
+import numpy as np
+
+def g_te(xA,lambda_sub, ideal_z):
+    gte = np.max(np.multiply(lambda_sub, np.abs(np.subtract(xA.objective_val, ideal_z) ) ) )
+    
+    return gte
